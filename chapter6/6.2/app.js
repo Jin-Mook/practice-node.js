a4bce5f43ab4bdba81e4a762d26ee4e965fb0856
jinmook <jink0608@naver.com>
2021-12-07 19:24:00 +0900

    미들웨어 알아보기

--- chapter6/6.2/app.js
+++ chapter6/6.2/app.js
@@ -25,17 +25,73 @@ app.use(session({
   name: 'session-cookie',
 }))
 
+const multer = require('multer')
+const fs = require('fs')
+
+try {
+  fs.readdirSync('uploads')
+} catch (error) {
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.')
+  fs.mkdirSync('uploads')
+}
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads/')
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname)
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext)
+    },
+  }),
+  limits: {fileSize: 5 * 1024 * 1024},
+})
+
+app.get('/upload', (req, res) => {
+  res.sendFile(path.join(__dirname, 'multipart.html'))
+})
+
+app.post('/upload',
+  upload.fields([{name: 'image1'}, {name: 'image2'}]),
+  (req, res) => {
+    console.log(req.files, req.body)
+    res.send('ok')
+  },
+)
+
+
 app.use((req, res, next) => {
   console.log('모든 요청에 다 실행됩니다.')
   next()
 })
 
 app.get('/', (req, res, next) => {
+  req.session.name = 'jjm'
+  console.log(req.sessionID)
+  console.log(req.session)
+  req.session.destroy()
+  console.log(req.session)
   res.sendFile(path.join(__dirname, 'index.html'))
   console.log('GET / 요청에서만 실행됩니다.')
-  // next()
+  // next('route')
 }, (req, res) => {
   throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
+})
+
+// 앞에 있는 라우터에서 next('route')를  이용한 경우
+app.get('/', (req, res, next) => {
+  res.send('next route')
+})
+
+app.get('/checkcookie', (req, res, next) => {
+  console.log(req.session)
+  if (req.session.num === undefined) {
+    req.session.num = 1
+  } else {
+    req.session.num += 1
+  }
+  res.send(`View : ${req.session.num}`)
 })
 
 app.use((err, req, res, next) => {
