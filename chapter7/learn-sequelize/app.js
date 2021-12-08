bed596616e89bf3a1fce1cc63e0441f928a3d0fc
jinmook <jink0608@naver.com>
2021-12-09 01:26:19 +0900

    간단한 댓글작성 기능 구현

--- chapter7/learn-sequelize/app.js
+++ chapter7/learn-sequelize/app.js
@@ -5,6 +5,9 @@ const nunjucks = require('nunjucks')
 const dotenv = require('dotenv')
 
 const {sequelize} = require('./models/index')
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+const commentsRouter = require('./routes/comments')
 
 dotenv.config()
 const app = express()
@@ -29,6 +32,10 @@ app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/comments', commentsRouter)
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`)
