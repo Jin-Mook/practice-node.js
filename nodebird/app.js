3f48b7548ed55f908fe743c4cf6ccf315dc24faa
jinmook <jink0608@naver.com>
2021-12-11 01:31:54 +0900

    multer패키지로 이미지 업로드 구현하기

--- nodebird/app.js
+++ nodebird/app.js
@@ -12,6 +12,7 @@ const passportConfig = require('./passport/index')
 dotenv.config()
 const pageRouter = require('./routes/page')
 const authRouter = require('./routes/auth')
+const postRouter = require('./routes/post')
 
 const app = express()
 passportConfig() // 패스포트 설정
@@ -33,6 +34,7 @@ sequelize.sync({ force: false })
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/img', express.static(path.join(__dirname, 'uploads')))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
@@ -51,6 +53,7 @@ app.use(passport.session())
 
 app.use('/', pageRouter);
 app.use('/auth', authRouter);
+app.use('/post', postRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
